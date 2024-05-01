9d570ddbe45d52a3c999a7ac55e0b20298c25396
HenryMaeda <henryteste25@gmail.com>
2024-04-30 21:45:30 -0300

    projeto final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner; 
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        // criei o objeto scanner para receber informações do usuario
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US) ; 
+
+        int numeroConta  ; 
+        String nomeCliente , agencia , sobrenomeCliente , nomeCompleto ; 
+        float saldo ; 
+        
+            System.out.println("Digite o seu numero: ");
+                numeroConta = scanner.nextInt() ; 
+
+            System.out.println("Digite o numero da sua agencia no formato XXX-X: ") ; 
+                agencia = scanner.next() ; 
+
+            System.out.println("Escreva seu nome: ") ; 
+                nomeCliente = scanner.next() ; 
+
+            System.out.println("Escreva seu sobrenome: ") ; 
+                sobrenomeCliente = scanner.next() ; 
+
+            System.out.println("Digite o saldo que voce deseja inserir na nova conta: ") ; 
+                saldo = scanner.nextFloat() ; 
+
+                nomeCompleto = nomeCliente + " " + sobrenomeCliente ; 
+            
+            System.out.println("Olá " + nomeCompleto + " " + "obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", " + " conta " + numeroConta + ", " +  "e seu saldo " + saldo + " " + "já está disponível para saque. "  );
+
+            scanner.close() ; 
+    } 
 }
